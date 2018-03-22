9b59159db795326af6093ad6ac879ba16e26a77e
Vanals <marco91vanali@gmail.com>
2018-03-22 12:27:33 +0000

    Update readme.md

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # GraphQL-GettinStarted
+
+## What is GraphQL?
+
+It is a query language for your API
+GraphQL is a query language for APIs and a runtime for fulfilling those queries with your existing data. GraphQL provides a complete and understandable description of the data in your API, gives clients the power to ask for exactly what they need and nothing more, makes it easier to evolve APIs over time, and enables powerful developer tools.
+
+## Result so far
+In order to move my first steps in this technology, I started this project to follow the GraphQL tutorial.
+First of all I've created a GraphQl schema, then a root to fill it in.
+At the end, using ExpressJS and graphqlHTTP I have been able to open the GraphiQL tool and create my API using the schema and the root I've created
+
+![](https://image.ibb.co/bVvGCx/Screen_Shot_2018_03_22_at_12_20_55.png)
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
-var { graphql, buildSchema } = require('graphql')
+// var { graphql, buildSchema } = require('graphql');
+var express = require('express');
+var graphqlHTTP = require('express-graphql');
+var { buildSchema } = require('graphql');
 
-//Construct a schema, using GraphQL schema language
+//Construct a schema, using GraphQL schema language (Schema of the API, which will be filled in)
 var schema = buildSchema(`
 type Query {
   hello: String
@@ -10,11 +13,25 @@ type Query {
 // The root provides a resolver function for each API endpoint
 let root = {
   hello: () => {
-    return 'Hello world!';
+    return 'Hello Marco!';
   },
 };
 
+var app = express();
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: root,
+  graphiql: true,
+}));
+
+app.listen(4000);
+console.log('Running a GraphQL API server at localhost:4000/graphql')
+// Once there u will find the GraphiQL tool.
+// There, on the left you can run the query 'hello'(function we create below)
+// running that you will use what is returned to fil in the schema, which will be returned on the right
+
+
 // Run the GraphQl query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+// graphql(schema, '{ hello }', root).then((response) => {
+//   console.log(response);
+// });
